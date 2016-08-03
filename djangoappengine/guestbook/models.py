57eb11623df41d91d0ba044a6ce9f8fcf4bb816b
Hung Vo <hungvt@aoi-sys.vn>
2016-08-03 16:14:45 +0700

    add Guestbook class and move get message method to Guestbook class

--- djangoappengine/guestbook/models.py
+++ djangoappengine/guestbook/models.py
@@ -16,11 +16,25 @@ class Greeting(ndb.Model):
     content = ndb.StringProperty(indexed=False)
     date = ndb.DateTimeProperty(auto_now_add=True)
 
-    def get_10_latest_message(self, guestbook_name):
-        greetings_query = Greeting.query(ancestor=guestbook_key(guestbook_name)).order(-Greeting.date)
-        greetings = greetings_query.fetch(10)
-        return greetings
+    # def get_10_latest_message(self, guestbook_name):
+    #     greetings_query = Greeting.query(ancestor=guestbook_key(guestbook_name)).order(-Greeting.date)
+    #     greetings = greetings_query.fetch(10)
+    #     return greetings
 
     def get_greetings_object(self, guestbook_name):
         greeting_object = Greeting(parent=guestbook_key(guestbook_name))
         return greeting_object
+
+    def add_new(self, guestbook_name, content, author):
+        greeting = self.get_greetings_object(guestbook_name)
+        greeting.content = content
+        greeting.author = author
+        greeting.put()
+
+class Guestbook(ndb.Model):
+    def get_lastest_greeting(self, guestbook_name,
+                             number_of_greeting=10):
+        greetings_query = Greeting.query(ancestor=guestbook_key(guestbook_name)).order(-Greeting.date)
+        greetings = greetings_query.fetch(number_of_greeting)
+
+        return greetings
--- djangoappengine/guestbook/views.py
+++ djangoappengine/guestbook/views.py
@@ -5,8 +5,11 @@ from django.views.generic.base import TemplateView
 from django.views.generic.edit import FormView
 
 from google.appengine.api import users
+from google.appengine.api import mail
+from google.appengine.ext import ndb
+from google.appengine.api import taskqueue
 
-from guestbook.models import Greeting, guestbook_key, DEFAULT_GUESTBOOK_NAME
+from guestbook.models import Greeting, Guestbook, guestbook_key, DEFAULT_GUESTBOOK_NAME
 
 import urllib
 
@@ -26,8 +29,10 @@ class MainPage(TemplateView):
         context['url_linktext'] = url_linktext
         context['form'] = form
         guestbook_name = self.request.GET.get('guestbook_name', DEFAULT_GUESTBOOK_NAME)
-        greetings_obj = Greeting()
-        greetings = greetings_obj.get_10_latest_message(guestbook_name)
+        # greetings_obj = Greeting()
+        # greetings = greetings_obj.get_10_latest_message(guestbook_name)
+        guestbook = Guestbook()
+        greetings = guestbook.get_lastest_greeting(guestbook_name, 10)
         context['greetings'] = greetings
         context['guestbook_name'] = guestbook_name
         return context
@@ -36,23 +41,30 @@ class SignPost(FormView):
     template_name = "guestbook/main_page_form.html"
 
     def post(self, request, **kwargs):
-        # import logging
-        # logging.warning("===== sign %r", request)
         if request.method == 'POST':
             form = SignForm(request.POST)
             if form.is_valid():
                 guestbook_name = request.POST.get('guestbook_name')
-                greetings_obj = Greeting()
-                greeting = greetings_obj.get_greetings_object(guestbook_name)
+                content = request.POST.get('content')
+                author = None
                 if users.get_current_user():
-                    greeting.author = users.get_current_user()
-
-                greeting.content = request.POST.get('content')
-                greeting.put()
+                    author = users.get_current_user()
+                greetings_obj = Greeting()
+                greetings_obj.add_new(guestbook_name, content, author)
+                if users.get_current_user():
+                    self.send_email()
                 context = self.get_context_data(**kwargs)
                 context['form'] = form
-                # import logging
-                # logging.warning("===== sign %r", context['guestbook_name'])
+                # guestbook_name = request.POST.get('guestbook_name')
+                # greetings_obj = Greeting()
+                # greeting = greetings_obj.get_greetings_object(guestbook_name)
+                # if users.get_current_user():
+                #     greeting.author = users.get_current_user()
+                #     self.send_email()
+                # greeting.content = request.POST.get('content')
+                # greeting.put()
+                # context = self.get_context_data(**kwargs)
+                # context['form'] = form
                 return HttpResponseRedirect('/?' + urllib.urlencode({'guestbook_name': guestbook_name}))
             else:
                 context = super(SignPost, self).get_context_data(**kwargs)
@@ -66,3 +78,11 @@ class SignPost(FormView):
     def get_context_data(self, **kwargs):
         context = super(SignPost, self).get_context_data(**kwargs)
         return context
+
+    @ndb.transactional
+    def send_email(self):
+        message = mail.EmailMessage(sender='hungvt@aoi-sys.vn',
+                                    to=users.get_current_user().email(),
+                                    subject="Your account has been approved",
+                                    body="""vi du noi dung""")
+        message.send()
